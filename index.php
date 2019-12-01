91bc8b64ad013bb764a877df35f597c00f028abf
leopnar <58390525+leopnar@users.noreply.github.com>
2019-12-01 21:47:17 +0900

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ a {
 <body>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Pencari Proxy © 2019</title>
+    <title>Rathalos/NizarE</title>
     <link rel="shortcut icon" href="https://resources.1337route.cf/favicon.ico" type="image/x-icon" />
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="https://colorlib.com/etc/cf/ContactFrom_v10/vendor/bootstrap/css/bootstrap.min.css">
